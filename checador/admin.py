4a13780ab8c4429f9ad6a66ac4d42dc640afae6d
MarioAV14 <mvaldez7497@gmail.com>
2019-08-06 14:52:34 -0500

    creación de modelos e instalación

---
+++ checador/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Registro, Incidencia
+# Register your models here.
+admin.site.register(Registro)
+admin.site.register(Incidencia)
---
+++ checador/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Registro(models.Model):
+    hora=models.DateTimeField(auto_now=False, auto_now_add=False)
+    nombre=models.CharField(max_length=50)
+    nivel=models.IntegerField()
+    observacion=models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.nombre
+    
+class Incidencia(models.Model):
+    nombre=models.CharField(max_length=50)
+    registros=models.ForeignKey("Registro", on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nombre
