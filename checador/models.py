6c8e2177b9cb9564fe157ebc189333e3a14afe91
reloadercf <elcfm1@gmail.com>
2019-08-07 14:22:02 -0500

    relacion señales y user

--- checador/models.py
+++ checador/models.py
@@ -1,19 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Registro(models.Model):
     hora=models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
-    nombre=models.CharField(max_length=50)
+    nombre=models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_registro')
     nivel=models.IntegerField()
     observacion=models.TextField(blank=True, null=True)
     def __str__(self):
-        return self.nombre
+        return self.nombre.username
     
 class Incidencia(models.Model):
-    nombre=models.CharField(max_length=50)
+    nombre=models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_incidencia')
     registros=models.ForeignKey("Registro", on_delete=models.CASCADE)
     def __str__(self):
-        return self.nombre
+        return self.nombre.username
 
 class Mese(models.Model):
     mes=models.CharField(max_length=50)
