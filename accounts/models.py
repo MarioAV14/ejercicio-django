f19ae4f1c2eac03fdae8cc6991134ed972209046
MarioAV14 <mvaldez7497@gmail.com>
2019-08-07 18:55:04 -0500

    error

---
+++ accounts/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .utils import unique_slug_generator
+from django.db.models.signals import pre_save, post_save
+
+# Create your models here.
+
+class Profile(models.Model):
+    usuario= models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile_usuario')
+    slug= models.SlugField(unique=True, blank=True)
+    cumple=models.DateField(blank= True, null=True)
+    def __str__(self):
+        return self.usuario.username
+
+    @property
+    def username(self):
+        return self.usuario.username
+
+def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+	if not instance.slug:
+		instance.slug = unique_slug_generator(instance)
+pre_save.connect(rl_pre_save_receiver, sender=Profile)
+
+@receiver(post_save, sender=User)
+def ensure_profile_exists(sender, **kwargs):
+    if kwargs.get('created', False):
+        Profile.objects.get_or_create(usuario=kwargs.get('instance'))
+    
--- ejercicio/settings.py
+++ ejercicio/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'checador', 
+    'accounts',
+    'oficinas',
 ]
 
 MIDDLEWARE = [
